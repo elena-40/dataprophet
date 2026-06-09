de958aefc050a0988b5db2c8ad68fb7c735713dc
elena-40 <elena.sapede@le-campus-numerique.fr>
2026-06-09 11:25:04 +0200

    Ajout de la route POST/api/predict (prediction factice)

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from fastapi import FastAPI
-# 1) on cree leapplication. title/description alimentent la doc auto (Swagger)
-app = FastAPI(title="DataProphet API", description="API de prediction de l'annee de plantation d'un arbre", version="1.0.0",)
 
-# 2) une route GET sur l'URL /health
-# le decorateur @app.get("/health") dit a FastAPI:
-# "quand quels'un fait  GET /health, execute la fonction en dessous"
+from schemas import CustomerFeatures, PredictionResponse
+
+app = FastAPI(
+    title="DataProphet API",
+    description="API de prédiction de l'année de plantation d'un arbre",
+    version="1.0.0",
+)
+
 
 @app.get("/health")
 def health_check():
-	# ce dictionaire Python sera automatiqument converti en JSON par fastAPI
-	return {"status": "OK", "message": "le service DataProphet est actif"}
+    return {"status": "ok", "message": "Le service DataProphet est actif"}
 
+
+@app.post("/api/predict", response_model=PredictionResponse)
+def predict(features: CustomerFeatures):
+    # ⚠️ Prédiction FACTICE pour l'instant — le vrai modèle arrive à l'étape 1.5
+    annee_factice = 1990.0
+    return PredictionResponse(
+        annee_plantation_estimee=annee_factice,
+        message=f"(factice) Arbre probablement planté vers {int(annee_factice)}",
+    )
